532ebeade4d16af43cd774e4bdd201300a7323c2
aampinar <98187277+aampinar@users.noreply.github.com>
2022-03-28 12:08:43 -0400

    Add JS

---
+++ index.js
@@ -0,0 +1,33 @@
+function forEach(array, functionToApply) {
+    for (let element of array) {
+      functionToApply(element)
+    }
+  }
+  
+  function logIt(element) {
+    console.log(element)
+  }
+   
+  function map (){
+    const numbers = [1,2,3]
+    return numbers.map(number => number * 2)
+  };
+  console.log(map());
+
+  function find (){
+    const groceryList = ['carrots', 'peas', 'brown rice', 'apples']
+    return groceryList.find(item => item === 'apples');
+  }
+  console.log(find());
+
+function filter (){
+    const names = [{
+    firstName: 'Kimberly',
+    lastName: 'Doe',
+},{
+    firstName: 'Elaine',
+    lastName: 'Smith',
+}]
+    return names.lastname.filter(person => person.lastname < 4)
+}
+console.log(filter());
